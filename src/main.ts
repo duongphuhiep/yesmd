219b10e0c0cf25a2544cb4f182a580d1432be62d
DUONG Phu-Hiep <duongphuhiep@gmail.com>
2019-12-22 08:10:19 +0100

    initial empty vuetify project

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import { Vuetify } from "vuetify"; //https://stackoverflow.com/a/58967068/347051
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
