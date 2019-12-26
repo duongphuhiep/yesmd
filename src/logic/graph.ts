d72b1476fc0b22aaa778bc1beedd1d715a3336ba
DUONG Phu-Hiep <duongphuhiep@gmail.com>
2019-12-27 00:42:41 +0100

    tmp

---
+++ src/logic/graph.ts
@@ -0,0 +1,74 @@
+//#region model
+export namespace model {
+    export interface Kind {
+        id: string;
+        name: string;
+        isLink: boolean;
+    }
+
+    export interface Relation {
+        id: string;
+        type: RelationType;
+        parent: string;
+        child: string;
+    }
+
+    export interface YModel {
+        Kinds: Kind[]
+        Relations: Relation[]
+    }
+
+    export enum RelationType {
+        /*1-N*/
+        Normal = 0,
+        /*0-N*/
+        Reference = 1,
+        /*1-1*/
+        Unique = 2,
+        /*0-1*/
+        UniqueOptional = 3,
+        /*Inheritance*/
+        Extension = 4,
+    }
+
+    export interface Dimension {
+        width: number;
+        height: number;
+    }
+
+}
+//#endregion
+
+const OneChar: model.Dimension = { width: 20, height: 36 };
+const Padding: model.Dimension = { width: 30, height: 30 };
+
+function estimateSize(textLength: number): model.Dimension {
+    return { width: OneChar.width * textLength, height: OneChar.height };
+}
+
+import dagre from "dagre";
+
+
+export function BuildDagreGraph(src: model.YModel): dagre.graphlib.Graph {
+    let g = new dagre.graphlib.Graph();
+
+    // Set an object for the graph label
+    //g.setGraph({});
+
+    src.Kinds.forEach(k => {
+        let dim = estimateSize(k.name.length);
+        g.setNode(k.id,
+            {
+                label: k.name,
+                width: dim.width + Padding.width,
+                height: dim.height + Padding.height
+            }
+        )
+    });
+
+    src.Relations.forEach(r => {
+        g.setEdge(r.child, r.parent);
+    });
+
+    return g;
+}
---
+++ tests/unit/logic/graphTest.spec.ts
@@ -0,0 +1,13 @@
+// import dagre from "dagre";
+// import {model, BuildDagreGraph} from "@/logic/graph.ts";
+
+// describe("GraphTest", () => {
+//     it("build the graph", async () => {
+//         try {
+//             let g: model.YModel = import("@@/data-sample/rbac.js");
+//             console.log(g);
+//         } catch (err) {
+//             console.error(err);
+//         }
+//     });
+// });
